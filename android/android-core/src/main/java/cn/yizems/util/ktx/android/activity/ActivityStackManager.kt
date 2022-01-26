33ffad0f97e6a5f167b1b3278de1b9af24a9a942
yizeliang <yizeliang@126.com>
2022-01-26 11:51:00 +0800

    ActivityStackManager 增加监听生命周期方法

--- android/android-core/src/main/java/cn/yizems/util/ktx/android/activity/ActivityStackManager.kt
+++ android/android-core/src/main/java/cn/yizems/util/ktx/android/activity/ActivityStackManager.kt
@@ -3,9 +3,13 @@ package cn.yizems.util.ktx.android.activity
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Application
+import androidx.lifecycle.Lifecycle
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.CopyOnWriteArrayList
 
 /**
  * Activity 队列管理
+ * 要确保一定在主线程运行
  */
 @SuppressLint("StaticFieldLeak")
 object ActivityStackManager {
@@ -14,11 +18,14 @@ object ActivityStackManager {
 
     private val list = mutableListOf<Activity>()
 
+    private val listeners = ConcurrentHashMap<Lifecycle.Event, CopyOnWriteArrayList<LifeCallback>>()
+
     private var curAct: Activity? = null
 
     fun getCurActivity() = curAct
 
     fun registerCallback(application: Application) {
+
         if (initialized) {
             return
         }
@@ -26,22 +33,36 @@ object ActivityStackManager {
 
             created = { activity, _ ->
                 list.add(activity)
+                dispatchListener(Lifecycle.Event.ON_CREATE, activity)
+            }
+
+            started = {
+                dispatchListener(Lifecycle.Event.ON_START, it)
             }
 
             resumed = { activity ->
                 curAct = activity
+                dispatchListener(Lifecycle.Event.ON_RESUME, activity)
             }
 
             stopped = { activity ->
                 if (curAct == activity) {
                     curAct = null
                 }
+                dispatchListener(Lifecycle.Event.ON_STOP, activity)
             }
             destroyed = { activity ->
                 list.remove(activity)
+                dispatchListener(Lifecycle.Event.ON_DESTROY, activity)
             }
         })
         initialized = true
+    }
+
+    private fun dispatchListener(event: Lifecycle.Event, activity: Activity) {
+        listeners[event]?.forEach {
+            it.invoke(activity)
+        }
     }
 
     fun exitApp() {
@@ -55,4 +76,36 @@ object ActivityStackManager {
      */
     @Synchronized
     fun getList() = list.toList()
+
+    /**
+     *
+     * @param event Event
+     * @param block Function1<[@kotlin.ParameterName] Activity, Boolean> return true will remove
+     */
+    fun addListener(
+        event: Lifecycle.Event,
+        callback: LifeCallback
+    ) {
+        val list = listeners[event] ?: CopyOnWriteArrayList()
+        listeners[event] = list
+        list.add(callback)
+    }
+
+    fun removeListener(
+        event: Lifecycle.Event,
+        callback: LifeCallback
+    ) {
+        val list = listeners[event] ?: return
+        list.remove(callback)
+    }
+
+    fun removeListener(
+        event: Lifecycle.Event
+    ) {
+        listeners.remove(event)
+    }
+
+    fun interface LifeCallback {
+        fun invoke(act: Activity): Boolean
+    }
 }
