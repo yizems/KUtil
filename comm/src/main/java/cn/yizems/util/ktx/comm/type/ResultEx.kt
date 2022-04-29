393282a3ef386bae13e5e185efb79bc52ec2dda3
yizeliang <yizeliang@gwmfc.com>
2022-04-29 13:01:57 +0800

    add kotlin ResultEx

---
+++ comm/src/main/java/cn/yizems/util/ktx/comm/type/ResultEx.kt
@@ -0,0 +1,89 @@
+package cn.yizems.util.ktx.comm.type
+
+/**
+ * Result 加强
+ * 主要是针对异常的处理
+ * [onFailure] 只会执行一次
+ * @param T
+ * @property result Result<T>
+ * @constructor
+ */
+class ResultWrapper<T>(
+    private val result: Result<T>
+) {
+
+    /** 异常是否已处理 */
+    var exceptionHandled = false
+
+    val isSuccess: Boolean
+        get() = result.isSuccess
+
+    val isFailure: Boolean
+        get() = result.isFailure
+
+    fun onSuccess(block: (T) -> Unit): ResultWrapper<T> {
+        result.onSuccess(block)
+        return this
+    }
+
+    fun onFailure(block: (Throwable) -> Boolean): ResultWrapper<T> {
+        result.exceptionOrNull()?.let {
+            if (!exceptionHandled) {
+                exceptionHandled = block(it)
+            }
+        }
+        return this
+    }
+
+
+    fun getResult(): Result<T> {
+        return result
+    }
+
+    fun getOrElse(default: (Throwable) -> T): T {
+        return result.getOrElse(default)
+    }
+
+    fun getOrDefault(default: T): T {
+        return result.getOrDefault(default)
+    }
+
+    fun getOrNull(): T? {
+        return result.getOrNull()
+    }
+
+    fun getOrThrow(): T {
+        return result.getOrThrow()
+    }
+
+
+    fun exceptionOrNull(): Throwable? {
+        return result.exceptionOrNull()
+    }
+
+    override fun toString(): String {
+        return result.toString()
+    }
+
+}
+
+
+fun <T> Result<T>.toResultWrapper(): ResultWrapper<T> {
+    return ResultWrapper(this)
+}
+
+/**
+ * 异常处理: 带类型
+ *
+ * @receiver ResultWrapper<T>
+ * @param block Function1<Throwable, Boolean>
+ * @return ResultWrapper<T>
+ */
+inline fun <T, reified R> ResultWrapper<T>.onFailureWith(block: (Throwable) -> Boolean): ResultWrapper<T> {
+    exceptionOrNull()?.let {
+        if (!exceptionHandled && it is R) {
+            exceptionHandled = block(it)
+        }
+    }
+    return this
+}
