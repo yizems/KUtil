5513a1df33bb191fe64ae22d173bc382e96393a7
yizeliang <yizeliang@gwmfc.com>
2022-06-29 18:32:13 +0800

    fun Any?.runOnNull 返回值修改为 receiver

--- comm/src/main/java/cn/yizems/util/ktx/comm/type/AnyEx.kt
+++ comm/src/main/java/cn/yizems/util/ktx/comm/type/AnyEx.kt
@@ -3,9 +3,8 @@ package cn.yizems.util.ktx.comm.type
 /**
  * Null 时执行 block
  */
-fun Any?.runOnNull(block: () -> Unit) = this.apply {
+fun <T> T?.runOnNull(block: () -> Unit) = this.apply {
     if (this == null) {
         block()
     }
 }
-
