08fef6bb08c4ace6a3cf27942188cbb0b93f4245
YiZe <yizeliang@126.com>
2022-07-10 23:50:10 +0800

    runOnNull 改为 inline 函数

--- comm/src/main/java/cn/yizems/util/ktx/comm/type/AnyEx.kt
+++ comm/src/main/java/cn/yizems/util/ktx/comm/type/AnyEx.kt
@@ -3,7 +3,7 @@ package cn.yizems.util.ktx.comm.type
 /**
  * Null 时执行 block
  */
-fun <T> T?.runOnNull(block: () -> Unit) = this.apply {
+inline fun <T> T?.runOnNull(block: () -> Unit) = this.apply {
     if (this == null) {
         block()
     }
